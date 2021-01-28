660c1511d59e0ac6d0ef9286a6c3ae24176d1202
xiang yi <xiangyisunn@gmail.com>
2021-01-28 18:36:24 +0100

    New state saved

---
+++ src/views/EventList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      Events Listing
+      <EventCard />
+      <BaseIcon />
+    </h1>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  components: {
+    EventCard
+  }
+};
+</script>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Showing Event #{{ id }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ["id"]
+};
+</script>
